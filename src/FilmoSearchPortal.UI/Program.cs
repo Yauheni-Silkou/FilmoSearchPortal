246ef232a32fb0e95ff3414a8942eea8acea3d93
Yauheni Silkou <yauheni.silkou@outlook.com>
2024-11-10 05:42:04 +0300

    chore(ui): add serilog in application

--- src/FilmoSearchPortal.UI/Program.cs
+++ src/FilmoSearchPortal.UI/Program.cs
@@ -1,8 +1,20 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddBusinessLayerConfiguration();
 builder.Services.AddControllersWithViews();
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Serilog/logs/log.txt", rollingInterval: RollingInterval.Hour)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
