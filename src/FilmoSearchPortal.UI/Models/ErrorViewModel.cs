003be71cff5c8466558a947197131f4b93da60b3
Yauheni Silkou <yauheni.silkou@outlook.com>
2024-11-12 09:57:03 +0300

    refactor: detete obsolete MVC project

--- src/FilmoSearchPortal.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FilmoSearchPortal.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
